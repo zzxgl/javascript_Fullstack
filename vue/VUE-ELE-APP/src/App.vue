99b98078aa27921257266811e089d9ca4ac270c5
zzxgl <291701005@qq.com>
2019-10-31 21:31:17 +0800

    饿了么

---
+++ vue/VUE-ELE-APP/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- 头部 -->
+    <v-header></v-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// 引入组件  注册组件
+import header from '@/components/header/header.vue'
+
+export default {
+  name: 'App',
+  
+  components:{
+    'v-header':header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ vue/VUE-ELE-APP/src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header">
+      hhhhhh
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
