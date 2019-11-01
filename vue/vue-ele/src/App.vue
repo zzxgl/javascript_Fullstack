edbae766526b0eac9ea1a39b93503263e012397d
zzxgl <291701005@qq.com>
2019-11-01 19:07:04 +0800

    stylus

---
+++ vue/vue-ele/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- 头部 -->
+    <v-header></v-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// 引入组件  注册组件 调用组件
+import header from '@/components/header/header.vue'
+
+export default {
+  name: 'App',
+
+  components:{
+    'v-header':header
+  }
+
+}
+</script>
+
+<style>
+
+</style>
+
