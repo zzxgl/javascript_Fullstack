a5826701f054ade4f8ed609856448af575de7fdf
zzxgl <291701005@qq.com>
2019-11-04 19:09:09 +0800

    update

--- node/pub-sub-node/event.js
+++ node/pub-sub-node/event.js
@@ -1,13 +1,14 @@
 //events是node内部的内置模块 也是实现其他模块的基础
 //工程化
-const Event = require('events')
-const ev = new Event();
-// 订阅
-ev.on('price',(a,b,)=>{
-    console.log(a,b,'涨价了')
+const Events = require('events')
+const ev = new Events();
+
+// 订阅 price自定义的名字 
+ev.on('price',(b)=>{
+    console.log(b,'涨价了')
 })
-ev.on('price',(a,b)=>{
-    console.log(a,b,'涨价了')
+ev.on('price',(a)=>{
+    console.log(a,'涨价了')
 })
 //发布
 ev.emit('price','猪肉','鸡肉');
