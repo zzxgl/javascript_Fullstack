479a2060d848afa74ae92b3e1f0fce669fc5e818
zzxgl <291701005@qq.com>
2019-11-14 19:03:42 +0800

    elementUI的使用

---
+++ vue/vue-elementUI/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Main from '@/components/Main'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/Main',
+      name: 'Main',
+      component: Main
+    }
+  ]
+})
