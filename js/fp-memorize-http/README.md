effbc19792cc05b639440c5fcba395fc50e8c856
zzxgl <291701005@qq.com>
2019-12-27 12:05:04 +0800

    etag&&last-modified

--- js/fp-memorize-http/README.md
+++ js/fp-memorize-http/README.md
@@ -24,7 +24,20 @@
   第二次请求：  
   ## 强缓存 
   1. 浏览器判断缓存有没有过期（依据 cache-control），如果没有直接从缓存里面取内容，不经过服务器
-  ## 协商缓存 Etag if-none-match 
-  2. 过期： Etag存在 -> 携带 if-none-match :abc 服务器决策 200 ？ 304 
-  3. Etag不存在 -> 携带 if-modefied-since:last-modified 200 ? 304
-  ps: Etag id-none-match 的优先级高于if-modified-since  last-modified 
+  2. 状态码 200 from memory/disk cache
+  3. expires: 绝对时间  2019122711:00:00  cache-control 相对时间 相对于上一次请求的时间和再次发出请求的时间  30*60
+  ## 协商缓存 Etag if-none-match  
+  1. Etag:一个能够标识文件内容唯一的东西 比如用哈希(hash)算法：MD5，sha-128,sha-256
+     哈希算法：同样的内容 hash完 之后结果一定是一样的 
+     不管hash任何东西 结果都是一个固定长度的字符串
+     md5('abc1234567') = xyz
+     md5('abc1234567') = xyz
+     md5(文件内容) = abc
+     第一次请求时， md5(文件内容) = 'hash1'
+     第二次请求时， md5(文件内容) = 'hash2'
+     last-modified 
+     第一次请求 服务器响应一个last-modified字段给浏览器 返回一个文件的最后修改时间
+     第二次请求 浏览器携带 if-modified-match 给服务端  再去获取下一个文件的最后修改时间
+  2. 过期： Etag存在 -> 携带 if-none-match :abc向服务器发起请求 -> 服务器决策 200(服务器进行响应) ? 304 (从缓存里面读取)
+  3. Etag不存在 -> 携带 if-modefied-since:last-modified -> 200 ? 304
+  ps: Etag if-none-match 的优先级高于if-modified-since  last-modified
--- js/fp-memorize-http/app.js
+++ js/fp-memorize-http/app.js
@@ -1,12 +1,13 @@
 var Koa = require('koa');
 var Router = require('koa-router');
 // var koaStatic = require('koa-static')
+const md5 = require('md5')
 
  
 var app = new Koa();
 var router = new Router();
 
-// app.use(koaStatic(__dirname))//把磁盘上面的文件路径  映射为 网络url   app1.js->localhost:8090/app1.js
+// app.use(koaStatic(__dirname))//把磁盘上面的文件路径  映射为 网络url   app1.js->localhost:9999/app1.js
 router.get('/', (ctx, next) => {  //定义根路由
   // ctx.router available
   ctx.body=`<!DOCTYPE html>
@@ -29,10 +30,24 @@ router.get('/app1.js',async (ctx) => {
   console.log('app1.js请求')
   const fs = require('fs')  //引入fs模块读取文件
   const content = fs.readFileSync('./app1.js','utf8')
+  const stat = fs.statSync('./app1.js')
+  const etag = md5(content)
   const time = Date.now() + 30000  //获取当前当前时间戳  缓存时间30秒有效
+  if (ctx.req.headers['if-none-match'] === etag) {
+    ctx.status = 304
+    ctx.body = ''
+    return 
+  }
+  if (ctx.req.headers['if-modified-since'] === stat.mtime) {
+    ctx.status = 305
+    ctx.body = ''
+    return 
+  }
   ctx.set('cache-control','public,max-age=30') //缓存30s有效 public文件的缓存可以在任何一个服务器上面缓存（代理）
 //   a -请求> ServerA -> ServerB
-  ctx.body = content
+  ctx.set('Etag',etag) //服务器响应 文件唯一资源标识给浏览器
+  ctx.set('Last-Modified',stat.mtime)  //服务器响应最新一次的修改文件时间
+  ctx.body = content  //响应体
 })
 app
   .use(router.routes())
